da120be148415cd1e4763d423040af59ebda3c14
ned <nedpike3@gmail.com>
2016-10-16 23:16:57 +1100

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,10 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
 
-  get 'page/index'
+  resources :users
 
+  get 'page/index'
+  post '/signup',  to: 'users#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
