56c4821c2a1fcb0d84698647e5aea1d8a958b17f
ned <nedpike3@gmail.com>
2016-10-17 18:00:51 +1100

    Implement Login

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'fog',                     '~> 1.38.0'
 gem 'will_paginate',           '~> 3.1.0'
 gem 'bootstrap-will_paginate', '~> 0.0.10'
 gem 'bootstrap-sass',          '~> 3.3.6'
+gem 'gravatar_image_tag'
 
 
 # Use Capistrano for deployment
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
   
 
+  
+
   root 'static_pages#home'
 
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-
   get '/signup', to: 'users#new'
-
-  resources :users
-
-  get 'page/index'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   post '/signup',  to: 'users#create'
+  get 'page/index'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
